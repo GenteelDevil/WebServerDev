df37ba93a12b983f53a0f04aecdb724f40772d45
OneShell <oneshell@pc.com>
2021-04-15 10:07:53 +0800

    add essential .h files

---
+++ mysql/mysql_connection_pool.h
@@ -0,0 +1,14 @@
+#ifndef _CONNECTION_POOL
+#define _CONNECTION_POOL
+
+#include <error.h>
+#include <mysql/mysql.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+#endif
